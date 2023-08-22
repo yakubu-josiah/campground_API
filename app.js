ed1ef9464f3e98de2cda5b29f8ff5f4a407deb4d
yakubu-josiah <jossysmothest007@gmail.com>
2023-08-22 20:10:31 +0100

    add user as middleware to templates using locals

--- app.js
+++ app.js
@@ -38,7 +38,10 @@ app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 seedDB();
 
-
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 //=================================//
 //      CAMPGROUNDS ROUTES         //
